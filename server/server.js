7c97c03038b54a905d53bdfee3319da4c19d7f16
Wayne-dot <wayneting.wc@gmail.com>
2024-08-11 00:41:27 -0400

    mongodb, env, mongoose setup

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express")
 
 
@@ -11,3 +13,15 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log("PORT 3000!!")
 })
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        // list for request from the client
+        app.listen(process.env.PORT, () => {
+            console.log(`Connect to MongoDB and listening on port ${process.env.PORT}`);
+        })
+    })
+
+    .catch((error) => {
+        console.log(error);
+    })
