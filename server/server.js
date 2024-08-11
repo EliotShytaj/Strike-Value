46e3e6d7a83664157c4bb48037646c11961d1702
Eliot Shytaj <eliotshytaj05@gmail.com>
2024-08-11 00:34:57 -0400

    minor server.js setup

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
 const express = require("express")
 
+
+//express app
 const app = express()
+
+app.get('/', (req, res) => {
+    res.json({mssg: "Hello World!"})
+  });
+
+app.listen(3000, () => {
+    console.log("PORT 3000!!")
+})
